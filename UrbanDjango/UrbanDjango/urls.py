68fa2b498e22c28b21611cb15273b92c90663c91
NiXoN <nick.gn@mail.ru>
2024-11-03 21:18:27 +0700

    second commit

--- UrbanDjango/UrbanDjango/urls.py
+++ UrbanDjango/UrbanDjango/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from task2.views import class_temp, func_temp
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('class/', class_temp),
+    path('func/', func_temp)
 ]
